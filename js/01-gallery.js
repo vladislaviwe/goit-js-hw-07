05b8a10b78871a010e7b41ed40d2ad22ab2dbc35
vladislaviwe <vladislaviwe@outlook.com>
2022-07-17 15:23:40 +0300

    Made the markup according to the homework
    The modal close button only works after the page is reloaded. Closing with the "Esc" button works without any problems.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = makeGalerryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function makeGalerryMarkup(items) {
+    return items.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join("");
+};
+
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImage = evt.target.classList.contains("gallery__image");
+    if (!isGalleryImage) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
+    instance.show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = makeGalerryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function makeGalerryMarkup(items) {
+    return items.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join("");
+};
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImage = evt.target.classList.contains("gallery__image");
+    if (!isGalleryImage) {
+        return;
+    }
+
+    return new SimpleLightbox(".gallery__item", {captionsData: "alt", captionDelay: 250});
+};
